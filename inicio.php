e130af2a630ff766b8faa4f42f6ec9c9a51d02b7
Iván Godoy <38009655+Ivan-godoy@users.noreply.github.com>
2018-07-27 02:24:37 -0600

    Cambios en el css y html para el menu, espacio de trabajo, header y footer

--- inicio.php
+++ inicio.php
@@ -10,59 +10,36 @@
 </head>
 <body>
 <header class="encabezado">
-    <div class="logo">
-
-    </div>
+    <img src="img/golazo.png" alt="" class="logo">
 </header>
 <section class="workspace">
     <nav class="navbar">
-        <ul>
-            <li><a href="#"> Item de Navegación 1 </a></li>
-            <li><a href="#"> Item de Navegación 2 </a></li>
+        <ul id="barra">
+            <li><a href="#"> Inicio</a></li>
+            <li><a href="#"> Item de Navegación 2 </a>
+                <ul>
+                    <li><a href="#"> sub 1</a></li>
+                    <li><a href="#"> sub 2 </a></li>
+                    <li><a href="#"> sub 3 </a></li>
+                </ul>
+            </li>
             <li><a href="#"> Item de Navegación 3 </a></li>
             <li><a href="#"> Item de Navegación 4 </a></li>
             <li><a href="#"> Item de Navegación 5 </a></li>
             <li><a href="#"> Item de Navegación 6 </a></li>
             <li><a href="#"> Item de Navegación 7 </a></li>
             <li><a href="#"> Item de Navegación 8 </a></li>
-            <li><a href="#"> Item de Navegación Etc </a></li>
+
         </ul>
     </nav>
     <section class="contenedor">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cupiditate facere id officia ratione, tempore.</p>
+
+
     </section>
 
 </section>
 <footer class="pie">
-
+    <p>Grupo Numero 1</p>
 </footer>
 </body>
 </html>
